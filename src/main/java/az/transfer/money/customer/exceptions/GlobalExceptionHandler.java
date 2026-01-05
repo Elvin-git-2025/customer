754f44b8f5a356c37d93f5a48c3ed089d078904f
Elvin Agazada <elvinagazad086a@gmail.com>
2026-01-05 16:01:59 +0400

    GlobalExceptionHandler was  moved to exceptions package

--- src/main/java/az/transfer/money/customer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/az/transfer/money/customer/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package az.transfer.money.customer.globalExceptionHandler;
+package az.transfer.money.customer.exceptions;
 
-import az.transfer.money.customer.exceptions.CustomerNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
